e5b4407b8a8ded6d1d8b0449bb1ec30efa15b3f5
PavelTitov16 <pashoktitov16@gmail.com>
2022-05-23 21:59:28 +0300

    fix: rewrite err mesages

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,13 +4,13 @@ const path = require('path');
 
 fs.readdir(path.join(__dirname, 'secret-folder'), function (err, files) {
     if (err) {
-        return console.error(error.message);
+        return console.error(err.message);
     }
     files.forEach((file) => {
         const filePath = path.join(__dirname, 'secret-folder', file);
         fs.stat(filePath, (err, file) => {
             if (err) {
-                return console.error(error.message);
+                return console.error(err.message);
             }
             if (file.isFile()) {
                 console.log(`${path.parse(filePath).name} - ${path.extname(filePath).slice(1)} - ${file.size / 1024}kb`)
