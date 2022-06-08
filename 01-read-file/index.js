e1fb6d15b4b592075065f4d36ef7a18adf8b947a
PavelTitov16 <pashoktitov16@gmail.com>
2022-06-08 11:22:53 +0300

    refactor: rewrite solution according to review by lector

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,21 +1,5 @@
-let fs = require('fs');
-
+const fs = require('fs');
 const path = require('path');
-let stream = new fs.ReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+const stream = new fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 
-stream.on('readable', () => {
-    let data = stream.read();
-    if (data != null) console.log(data);
-});
-
-stream.on('error', (err) =>  {
-    if (err.code === 'ENOENT') {
-        console.log("File not found");
-    } else {
-        console.error(err);
-    }
-});
-
-stream.on('end', () => {
-    console.log("THE END");
-});
+stream.pipe(process.stdout);
